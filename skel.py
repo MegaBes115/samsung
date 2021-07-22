9ef6a0245bca8bfa7fbcfef2c103d709f5c66dc7
Timur Mangliev <tigrmango@gmail.com>
2021-07-22 15:41:22 +1000

    Обновил skel.py, когда будут функции, первый этап вроде должен быть готов

--- skel.py
+++ skel.py
@@ -1,14 +1,15 @@
-from typing import Union
-from scipy import integrate
+import colorsys
+from time import sleep
+from typing import Tuple, Union
+
 import cv2 as cv
 import numpy as np
+from scipy import integrate
+
 import pymurapi as mur
-import colorsys
-import time
 
 
-
-def find_gates_err(image: np.ndarray) -> float: #TODO: This thing finds error between needed position of gates' middlepoint and its current position
+def find_gates_err(image: np.ndarray) -> Tuple(float, bool): #TODO: This thing finds error between needed position of gates' middlepoint and its current position
     """Finds error between gates' middlepoint position and center of videofeed
 
     Args:
@@ -16,6 +17,7 @@ def find_gates_err(image: np.ndarray) -> float: #TODO: This thing finds error be
 
     Returns:
         float: Error
+        bool: True if gates are found
     """
     pass
 
@@ -29,6 +31,7 @@ def stabilize_yaw(target: float, possible_err: float, additional_fwd: float) -> 
     Returns:
         bool: Stabilizing status, True if stabilized
     """
+    pass
 
 
 def get_auv_image(auv: mur.auv) -> np.ndarray:
@@ -39,10 +42,37 @@ def get_auv_image(auv: mur.auv) -> np.ndarray:
     Returns:
         np.ndarray: Image
     """
+    pass
+
+def spot_marker(image: np.ndarray) -> bool:
+    """Find marker, green or blue
+
+    Args:
+        image (np.ndarray): Image input, not cropped
+
+    Returns:
+        bool: True if green
+    """
+    pass
+
+def circle_marker(green: bool):
+    """Goes around marker as in the reglament
+
+    Args:
+        green (bool): True if marker is green
+    """
+
+    pass
 if __name__ == "__main__":
-    auv = mur.mur_init()
-    success, err = find_gates_err(get_auv_image(auv))
-    if success:
-        while not stabilize_yaw(err, 1, 50):
-            success, err = find_gates_err(get_auv_image(auv))
+    auv = mur.mur_init() # Init
+    err, found = find_gates_err(get_auv_image(auv)) # Find gates
+    if found:
+        while not stabilize_yaw(err, 1, 50) and found:
+            err, found = find_gates_err(get_auv_image(auv)) # While gates are found, go in between
+    auv.set_motor_power(0,100) # ↓
+    auv.set_motor_power(1,100) # Pass gates
+    sleep(2)                   # ↑
+    circle_marker(spot_marker(get_auv_image(auv)))
+    
+        
         
